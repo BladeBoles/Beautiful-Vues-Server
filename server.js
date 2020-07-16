18f75b8bcb556856babd1e6f595d8898a58d5247
Blade <bladerboles@gmail.com>
2020-07-15 21:29:40 -0600

    Prepare for deploy

--- server.js
+++ server.js
@@ -19,4 +19,5 @@ server.use(cors());
 server.use('/quotes', router)
 server.use('/favorites', favoritesRouter)
 
-server.listen(3000, () => console.log("Server listening on port 3000!"));
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log("Server online!"));
